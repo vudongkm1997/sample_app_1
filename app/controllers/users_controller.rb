5de2205a8a140fab2c8b142afa053433c8fa0089
Vu Van Dong <vudongkm1997@gmail.com>
2017-12-13 14:24:19 +0700

    finish chap 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new user_params
-
     if @user.save
+      log_in @user
       flash[:success] = t :flash_success
       redirect_to @user
     else
